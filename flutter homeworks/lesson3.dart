cfab6b22c1937511427cb9f08171885d46542777
Aizhana-D <dzhumabekovait@gmail.com>
2021-03-15 01:16:41 +0600

    update dart

---
+++ flutter homeworks/lesson3.dart
@@ -0,0 +1,12 @@
+void main() {
+  /// main is the entry point of program
+  var lst = [1, 2, 3];
+  /*  
+  It prints  
+  the whole list  
+  at once  
+  */
+  //or//
+  //It prints  the whole list at once//
+  print(lst);
+}
