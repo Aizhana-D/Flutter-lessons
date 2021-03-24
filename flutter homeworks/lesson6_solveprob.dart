91238568d3ddd5ee920536e04747cb481412d6e5
Aizhana-D <dzhumabekovait@gmail.com>
2021-03-25 00:31:32 +0600

    update 6sp

--- flutter homeworks/lesson6_solveprob.dart
+++ flutter homeworks/lesson6_solveprob.dart
@@ -9,9 +9,7 @@ void main() {
   /// sp means covert to array
   print(a);
   var b = [];
-  for (int i = a.length - 1;
-      i >= 0;
-      i--) //for (var i = 1; i <= 1; i--) //infinity
+  for (int i = a.length - 1;i >= 0;  i--) //for (var i = 1; i <= 1; i--) //infinity
     //{  add
     b.add(a[i]);
   // added a to b //
@@ -38,7 +36,7 @@ void main() {
 }
 //--end 1--//
 
-//--2--//
+//--**2 Задача с массивом чисел--//
 void main() {
   List a = [2, 7, 11, 15];
   int b = 9;
@@ -63,9 +61,10 @@ void main() {
     }
   }
 }
-//--end 2--//
+//--**end 2 Задача с массивом чисел--//
 
 //--3 Задача с массивом чисел--//
+//--lazy way :)
 void main3() {
   var l1 = [1, 2, 4];
   var l2 = [1, 3, 4];
@@ -73,50 +72,72 @@ void main3() {
   newMyList.sort(); //sort
   print(newMyList);
 }
-
+//--end lazy way
 //--3
-void main31() {
-  var l1 = [];
-  var l2 = [];
-  var newMyList = l1 + l2;
-  print(newMyList);
+void main() {
+  var l1 = [4, 2, 1, 1, 3, 4]; // here used swap and bubble function 
+   int a; // need for swap, it's like an empty busket
+  var r = l1;
+  for (int i = 0; i < r.length - 1; i++) { //каждый элемент берём из листа
+    for (int y = i + 1; y < r.length - 1; y++) {// каждый элемент берём но со второго
+      // i каждый y со следующим
+      if (r[i] > r[y]) {
+        //каждый элемент сравниваем со следующим//если больше то меняй //swap or bubbble sort !!! quick sort посмотреть
+        //первый сравниваем со вторым
+        a = r[i]; // первый элемент в пустышке
+        r[i] = r[y]; // второй в первой
+        r[y] = a; //пустышку во второй
+      }
+    }
+  }
+  print(r); // сорт закончен
 }
 //--end 3 Задача с массивом чисел--//
 
-//--4--//
+//--**4**--//
+//-- not correct
 void main4() {
-  var s = "()";
-
-  if (s == "()") {
-    //!!!(== )equally равно;!!! запомнить ошибка если (=)
-    print("true");
-  } else {
-    print("false");
-  } // result is true
-}
-
-//--4
-void main() {
   var s = "([)]";
 
   if (s == "([)]") {
-    //
+
     print("false");
   } else {
     //!!!(== )equally равно;!!! запомнить ошибка если (=)
     print("true");
   } // result is false
 }
+//end correct
+//--** 4 **--
+import 'dart:io';
 
-//--4
-void main() {
-  var s = "(]";
-
-  if (s != "(]") {
-    //!!!(== )equally равно;!!! запомнить ошибка если (=)
-    print("true");
-  } else {
-    print("false");
-  } // result is true
+void main41() {
+  while (true) {
+    print("Enter a string");
+    String a = stdin.readLineSync()!;
+    List stack = [] ///stack -структура данных, которая следует определенному порядку выполнения операций (ведро)
+    bool s = true;
+    String temp; //пустышка из вернего элемента ведра напримем:первый элемент должен выйти последним
+    for (int i = 0; i < a.length; i++) { // каждый элемент строки
+      if (a[i] == "[" || a[i] == "{" || a[i] == "(")
+        stack.add(a[i]); //stack first in last out
+      else if (a[i] == "]" || a[i] == "}" || a[i] == ")") {
+        if (stack.isEmpty) { //если ведро пустое
+          s = false;
+          break;
+        } else {
+          temp = stack.removeLast(); //пустышка из вернего элемента ведра 
+          if ((temp == "[" && a[i] != "]") ||
+              (temp == "{" && a[i] != "}") ||
+              (temp == "(" && a[i] != ")")) {
+            s = false;
+            break;
+          }
+        }
+      }
+    }
+    if (stack.isNotEmpty) s = false;
+    print("result(" + a + ")="+ s.toString()); // bool сделать toString распечатать
+  }
 }
-//-- end 4--//
+//--** end 4**--//
--- flutter homeworks/test.dart
+++ flutter homeworks/test.dart
@@ -1,11 +1,20 @@
 void main() {
-  var s = "(]";
-
-  if (s != "(]") {
-    //
-    //!!!(== )equally равно;!!! запомнить ошибка если (=)
-    print("equal");
-  } else {
-    print("not equal");
-  } // result is true
+  var l1 = [4, 2, 1, 1, 3, 4]; // here used swap and bubble function
+  int a; // need for swap, it's like an empty busket
+  var r = l1;
+  for (int i = 0; i < r.length - 1; i++) {
+    //каждый элемент берём из листа
+    for (int y = i + 1; y < r.length - 1; y++) {
+      // каждый элемент берём но со второго
+      // i каждый y со следующим
+      if (r[i] > r[y]) {
+        //каждый элемент сравниваем со следующим//если больше то меняй //swap or bubbble sort !!! quick sort посмотреть
+        //первый сравниваем со вторым
+        a = r[i]; // первый элемент в пустышке
+        r[i] = r[y]; // второй в первой
+        r[y] = a; //пустышку во второй
+      }
+    }
+  }
+  print(r); // сорт закончен
 }
