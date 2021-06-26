33e8b3eaf2ded3c3f653e68f25c5e052c8a2afc6
Aizhan-D <dzhumabekovait@gmail.com>
2021-06-26 10:16:49 +0600

    hms

--- flutter homeworks/animated_cont/android/app/src/main/kotlin/com/example/animation_bloc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.animation_bloc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter homeworks/animated_cont/lib/bloc/random_block_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'random_block_bloc.dart';
-
-@immutable
-class RandomBlockEvent {}
-
-class RandomContainerEvent extends RandomBlockEvent {}
-
-class ResetEvent extends RandomBlockEvent {}
